bdb9955c90c6b9c11c61e4559414f1e73b1254ba
Athmajith Johnson <54813630+athmajith-johnson@users.noreply.github.com>
2025-08-04 23:10:12 +0800

    Add files via upload

---
+++ links.js
@@ -0,0 +1,127 @@
+// Site data for AR Services Web Portal
+// Last updated: July 2025
+
+/*
+ * Structure of each entry:
+ * {
+ *     "name": "Site Name",
+ *     "url": "Site URL",
+ *     "description": "Site Description"
+ * }
+ */
+
+const siteData = {
+    // AR Local Services
+    "AR-Local": [
+        {
+            "name": "Harmony",
+            "url": "http://inprod105.in.ray.group:8081/login.html",
+            "description": "Portal for all HR services."
+        },
+        {
+            "name": "Paysquare",
+            "url": "https://mypayroll.paysquare.com/",
+            "description": "Portal for salary and payroll management."
+        },
+        {
+            "name": "Windchill",
+            "url": "https://rfq.araymond.com:8091/iway/app/",
+            "description": "All Product Drawing related data."
+        },
+        {
+            "name": "VMS",
+            "url": "http://inprod108/VMS/Default.aspx",
+            "description": "Visitor Management System portal."
+        },
+        {
+            "name": "Ascent",
+            "url": "http://10.190.12.11/AscentESS/Auth/PreLogin.aspx",
+            "description": "Portal for attendance management."
+        },
+        {
+            "name": "Pathfinder",
+            "url": "https://pathfinder.araymond.com/",
+            "description": "Product related data and resources."
+        },
+        {
+            "name": "Material Master",
+            "url": "https://inprod102.in.ray.group/matmst/login.php/?gg=Kh2PyCHsWJzomxT",
+            "description": "New Material Creation and Management portal."
+        },
+        {
+            "name": "Glpi",
+            "url": "https://servicedesk.ar.ray.group/front/helpdesk.public.php",
+            "description": "Portal for raising tickets and IT support."
+        },
+        {
+            "name": "Liquidfiles",
+            "url": "https://liquidfiles.araymond.com/link",
+            "description": "File sharing and transfer portal."
+        },
+        {
+            "name": "ARise",
+            "url": "https://inprod102.in.ray.group/arise/",
+            "description": "Suggestions and ideas portal for employees."
+        },
+        {
+            "name": "Learning Feedback Portal",
+            "url": "http://inprod105.in.ray.group:8081/Learning/MyLearning.aspx",
+            "description": "Training and learning feedback portal."
+        }
+    ],
+    // New Product Development - Metal
+    
+    "NPD-Metal": [
+        {
+            "name": "Leave Planner",
+            "url": "https://raynet1.sharepoint.com/sites/C-IN-MetalProjects/Shared%20Documents/General/Leave%20planner-Metal%20Engg.xlsx",
+            "description": "For leave planning and management."
+        }
+    ],
+    // TDD - Tool Design and Development
+
+    "TDD": [
+        {
+            "name": "Part-Wise Quote",
+            "url": "https://raynet1-my.sharepoint.com/personal/athmajith_johnson_araymond_com/Documents/Microsoft%20Teams%20Chat%20Files/Quotation%20details_Partwise.xlsx",
+            "description": "RFQ details for part-wise quotations."
+        }
+    ],
+    // AQ - Advance Quality
+
+    "AQ": [
+        {
+            "name": "QPCR",
+            "url": "https://raynet1.sharepoint.com/:f:/r/sites/C-IN-MetalProjects/Shared%20Documents/General/QPCR?csf=1&web=1&e=H6ecEP",
+            "description": "All QPCR files."
+        }
+    ],
+
+    // Process Engineering
+//    "PE": [
+//        {
+//            "name": "Material Master",
+//            "url": "https://inprod102.in.ray.group/matmst/login.php/?gg=Kh2PyCHsWJzomxT",
+//            "description": "Material Master portal"
+//        },
+//        {
+//            "name": "Glpi",
+//            "url": "https://servicedesk.ar.ray.group/front/helpdesk.public.php",
+//            "description": "ticket raising portal"
+//        }
+//    ],
+
+    // Product Development Engineering
+//    "PDE": [
+//        {
+//            "name": "Liquidfiles",
+//            "url": "https://liquidfiles.araymond.com/link",
+//            "description": "Liquidfiles portal"
+//        },
+//        {
+//            "name": "ARise",
+//            "url": "https://inprod102.in.ray.group/arise/",
+//            "description": "ARise portal"
+//        }
+//    ]
+};
